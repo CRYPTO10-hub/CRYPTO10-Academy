84280c8f68da97f4c1196dea2569b3ced4d4a17d
MOHAMED COULIBALY <aboko7162@gmail.com>
2025-05-04 01:40:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+```javascript
+const mobileMenu = document.getElementById('mobile-menu');
+const nav = document.querySelector('.nav');
 
+mobileMenu.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+```
